d4dcd37308b6b9710eb2db47f42bdd8a601b5113
marimaritian <105123923+marimaritian@users.noreply.github.com>
2023-04-23 17:24:12 -0300

    Comentarios do testebench final

--- MemoriaREg/testbench.v
+++ MemoriaREg/testbench.v
@@ -46,18 +46,51 @@ module testbench;
 
     initial 
     begin
+        //Para simular, precisa-se dar o sinal de clock(que será 0 ou 1), também o sinal do somador(0 se for soma ou 1 se for subtração),
+        //Ainda, deve-se fornecer o b_tb que é endereço que será somado com zero(a_tb), e que será o endereço da memória. Por fim, também
+        //é necessário fornecer o rw_tb que irá como endereço do registrador no momento do load, e também fornecer o ra_tb que será o endereço
+        //onde a informação será lida no registrador. Por fim, é necessário fornecer o we do registrador(weReg_tb) e o we da memória(weReg_tb).
+
         clock_tb = 0;
         sinal_tb=0;
         a_tb = 0;
-        b_tb = 16;
-        weReg_tb = 1;
-        Rw_tb = 10;
-        Ra_tb=10;
+        b_tb = 7;
+        Rw_tb = 0;
+        Ra_tb=0;
         #10
         #10
+
+//*******************************Abaixo é a simulação do store na memória: ********************************************************************
         weReg_tb = 0;
         #10
-             $display(doutA_tb);
+            //Aqui é a demonstração do conteudo do registrador que futuramente será escrito na memória.
+            // Já que o we do registrador esta zero, entao o douta vale 32 que eh o valor de registrador[0]
+            $display(doutA_tb);
+
+        weMem_tb=1;
+            //Aqui foi escrito o conteudo de registrador[0] no registrador_memoria[7], pois o somador passou o valor 7,
+        #10
+        #10
+        weMem_tb=0;
+        #10
+            //Aqui o display abaixo revela o que há na memoria na posição 7 e comprova o sucesso da operação store.
+            $display(doutMem_tb);
+
+
+//*******************************Abaixo é a simulação do load no registrador: ********************************************************************
+    
+        #10
+        Rw_tb = 12;
+
+        weReg_tb=1;
+            //Aqui foi carregado os dados do doutMem_tb(que vale 32) no registrador da posição 12
+        #10
+
+        Ra_tb=12;
+        weReg_tb=0;
+        #10
+            $display(doutA_tb); 
+            //Esse display serve para provar que foi carregado os dados do doutMem_tb no registrador da posição 12
         
       
        
